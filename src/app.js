0e5c274e9da8edffbac6d6bdfc52f5a871c9e55a
TovaTheCode <116457722+TovaTheCode@users.noreply.github.com>
2023-03-19 06:51:14 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 app.get('/status', function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get('/invite', function(res, req){
+    req.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands')
+});
 app.get("*", function(req,res){
     res.redirect('/status')
 });
