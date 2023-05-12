15a92f5c757a4df86fd8f920a0eaf0832476d580
TovaTheCode <uglyr3b+github@gmail.com>
2023-05-12 07:55:24 -0800

    new /me

--- src/app.js
+++ src/app.js
@@ -15,9 +15,6 @@ app.get('/me' ,function(req, res){
   res.sendFile(__dirname + '/portweb/index.html');
 
 })
-app.get('/invite', function(res, req){
-    req.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands')
-});
 app.get("*", function(req,res){
     res.redirect('/status')
 });
