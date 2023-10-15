0c26f908d50be0b1ee9ff113f0069b0a8e24d5c2
Tova <116457722+TovaIsME@users.noreply.github.com>
2023-10-15 10:21:50 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,59 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const PORT = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname + '/portweb')));
 app.get('/status', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-function inversePermutation(arr) {
-  const size = arr.length;
-  const result = [];
+app.get('/invite', function(res, req){
+    req.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands')
 
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      if (arr[j] === i + 1) {
-        result.push(j + 1);
-        break;
-      }
-    }
-  }
-
-  return result;
-}
-
-app.use(express.json());
-
-app.get('/math/daiva/is/cute/inverse-p', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index2.html'));
-});
-
-app.post('/inverse-permutation', (req, res) => {
-  const inputArray = req.body.inputArray;
-
-  if (!Array.isArray(inputArray)) {
-    return res.status(400).json({ error: 'Input should be an array ex. [1,2,3,4,5].' });
-  }
-
-  const result = inversePermutation(inputArray);
-  res.json({ result }); // Send the result as JSON
-});
-
-
-app.get('/invite', function(req, res){
-    res.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands');
-});
-
+})
 app.get('/me' ,function(req, res){
   res.sendFile(__dirname + '/portweb/index.html');
-})
 
-app.get("*", function(req, res){
-    res.redirect('/status');
+})
+app.get("*", function(req,res){
+    res.redirect('/status')
 });
 
 app.listen(PORT, () => {
-  console.log('app is online');
-});
+  console.log('app is online')
+})
