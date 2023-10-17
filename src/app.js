7faaaf63fd8f281b678a1605f401f0854a0198bd
Tova <116457722+TovaIsME@users.noreply.github.com>
2023-10-17 10:39:14 +0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,38 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const PORT = 3000;
-app.use(express.static(path.join(__dirname + '/portweb')));
-app.get('/status', function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.get('/invite', function(res, req){
-    req.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands')
-
-})
-app.get('/me' ,function(req, res){
-  res.sendFile(__dirname + '/portweb/index.html');
-
-})
-app.get("*", function(req,res){
-    res.redirect('/status')
-});
-function inversePermutation(arr) {
-  const size = arr.length;
-  const result = [];
-
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      if (arr[j] === i + 1) {
-        result.push(j + 1);
-        break;
-      }
-    }
-  }
-
-  return result;
-}
+app.use(express.static(path.join(__dirname + '/portweb'));
 
 app.use(express.json());
 
@@ -50,6 +20,23 @@ app.post('/inverse-permutation', (req, res) => {
   const result = inversePermutation(inputArray);
   res.json({ result }); // Send the result as JSON
 });
+
+app.get('/status', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/invite', function(req, res){
+    res.redirect('https://discord.com/api/oauth2/authorize?client_id=938861493879967815&permissions=268512278&scope=bot%20applications.commands');
+});
+
+app.get('/me', function(req, res){
+  res.sendFile(__dirname + '/portweb/index.html');
+});
+
+app.get("*", function(req, res){
+    res.redirect('/status');
+});
+
 app.listen(PORT, () => {
-  console.log('app is online')
-})
+  console.log('app is online');
+});
